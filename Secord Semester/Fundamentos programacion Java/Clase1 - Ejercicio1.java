b1c3f4fa8329a8c58584e032ea4f1dec72dc50de
• 🫀 ⁾⁾﹕ 𝐌 𝐢 𝐫 𝐢 ⌇ <160249239+1tsm1ri@users.noreply.github.com>
2025-01-27 17:20:28 -0500

    Add files via upload

---
+++ Secord Semester/Fundamentos programacion Java/Clase1 - Ejercicio1.java
@@ -0,0 +1,23 @@
+
+public class Main {
+	public static void main(String[] args) {
+	    
+	    String nombre = "Mariana Lemus Esguerra";
+		int edad = 19; 
+		float estatura = 1.54f;
+		double peso = 45.7;
+		char talla = 'M';
+		final long NUMERO_CELULAR = 3006204138L;
+		
+		System.out.println(nombre);
+		System.out.println(edad + " anios");
+		System.out.println(estatura);
+		System.out.println(peso + "kg");
+		System.out.println(talla);
+		System.out.println(NUMERO_CELULAR);
+		
+		System.out.printf("Mi nombre es %s y mi edad es: %d \n", nombre, edad);
+		System.out.printf("Mi estatura es: %.2f", estatura);
+		
+	}
+}
